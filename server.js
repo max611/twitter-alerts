fbbf8bad171ccb7c7e34675e5e500fbf21016b0b
Max St-Onge <maxstonge@gmail.com>
2020-06-26 17:53:13 -0400

    Save and display tweet on the go

---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const redis = require("redis");
+const client = redis.createClient();
+var bodyParser = require('body-parser');
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname);
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get('/', (req, res) => {
+  res.render('views/index');
+});
+
+app.get('/add_tweet', (req, res) => {
+  res.render('views/add_tweet');
+});
+
+app.get('/browser_source', (req, res) => {
+  res.render('views/browser_source');
+});
+
+app.post('/add_tweet', (req, res) => {
+	client.rpush('tweets', req.body.url_field);
+	res.redirect('/logs')
+});
+
+app.get('/logs', async (req, res) => {
+	tweets = []
+	client.lrange('tweets', 0, -1, function(error, result) {
+	  if (error) {
+	      console.error(error);
+	  } else {
+	  	tweets = result
+	    res.render('views/logs', {tweets: tweets});
+	  }
+	});
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('tweet', (msg) => {
+    io.emit('tweet', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
