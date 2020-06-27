2a33cd34200c5a52451b2fe037d42979585af617
Max St-Onge <maxstonge@gmail.com>
2020-06-27 15:23:56 -0400

    update index and admin panel style

---
+++ public/js/admin.js
@@ -0,0 +1,26 @@
+$(function () {
+	var socket = io();
+	$('form').submit(function(e){
+	  e.preventDefault(); // prevents page reloading
+	  var clicked_btn = $(document.activeElement).attr('name');
+	  checked_tweets = $('.tweets:checkbox:checked')
+	  tweet_values = []
+	  for (index = 0; index < checked_tweets.length; index++) { 
+	    tweet_values.push(checked_tweets[index].value)
+		}
+	    if(clicked_btn == 'show'){
+	      socket.emit('tweet', tweet_values);
+	      $('#sucess-banner').html(`${tweet_values.length} tweets sent to the browser source.`);
+	      $("#sucess-banner").fadeIn(1000).delay(3000).fadeOut(1000)
+	      return false;
+	  } else{
+	  	if(tweet_values.length == 0){
+	  		return;
+	  	}
+	  	$.post('/tweets/delete', {tweets: tweet_values}).done(function(response){
+				   location.reload();
+				});
+	  }
+	  
+	});
+});
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const redis = require("redis");
-const client = require('redis').createClient(process.env.REDIS_URL);
+const redisClient = require('redis').createClient(process.env.REDIS_URL);
 
 var bodyParser = require('body-parser');
 
-client.on("error", function(error) {
+redisClient.on("error", function(error) {
   console.error(error);
 });
 
@@ -16,33 +16,57 @@ app.set('port', PORT);
 app.set('views', __dirname);
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('views/index');
+  var hostname = process.env.HOSTNAME || 'localhost:3000';
+  res.render('views/index', {hostname: hostname});
 });
 
-app.get('/add_tweet', (req, res) => {
+app.get('/tweets/add', (req, res) => {
   res.render('views/add_tweet');
+});
+
+app.post('/tweets/add', (req, res) => {
+  redisClient.rpush('tweets', req.body.url_field);
+  res.redirect('/tweets/add')
+});
+
+app.get('/admin', (req, res) => {
+  tweets = []
+  redisClient.lrange('tweets', 0, -1, function(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+      tweets = result
+      res.render('views/admin', {tweets: tweets, libs: ['admin']});
+    }
+  });
+});
+
+app.post('/tweets/delete', (req, res) => {
+  console.log('tweets delete route')
+  const tweets = req.body.tweets;
+  tweets.forEach(async function(tweet) {
+    redisClient.lrem('tweets', -1, tweet);
+  })
+
+  res.sendStatus(200)
 });
 
 app.get('/browser_source', (req, res) => {
   res.render('views/browser_source');
 });
 
-app.post('/add_tweet', (req, res) => {
-	client.rpush('tweets', req.body.url_field);
-	res.redirect('/logs')
-});
-
-app.get('/logs', async (req, res) => {
+app.get('/tweets', async (req, res) => {
 	tweets = []
-	client.lrange('tweets', 0, -1, function(error, result) {
+	redisClient.lrange('tweets', 0, -1, function(error, result) {
 	  if (error) {
 	      console.error(error);
 	  } else {
 	  	tweets = result
-	    res.render('views/logs', {tweets: tweets});
+	    res.render('views/tweets', {tweets: tweets});
 	  }
 	});
 });
