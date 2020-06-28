20abc353b3e183cb35c214e928c54021429ef859
Max St-Onge <maxstonge@gmail.com>
2020-06-27 20:11:45 -0400

    add prod db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,16 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "max",
-  PASSWORD: "123456",
-  DB: "testdb"
-};
+if(process.env.NODE_ENV === 'production') {
+    module.exports = {
+		  HOST: process.env.DB_HOST,
+		  USER: process.env.DB_USERNAME,
+		  PASSWORD: process.env.DB_PASSWORD,
+		  DB: process.env.DB_DATABASE
+		};
+} else {
+  module.exports = {
+	  HOST: "localhost",
+	  USER: "max",
+	  PASSWORD: "123456",
+	  DB: "testdb"
+	};
+}
+
