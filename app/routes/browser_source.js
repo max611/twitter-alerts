9340d14e56ad1470e9c20300466026bf7a5cc76b
Max St-Onge <maxstonge@gmail.com>
2020-07-18 16:20:04 -0400

    use room to fetch settings

--- app/routes/browser_source.js
+++ app/routes/browser_source.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const settingsCtrl = require("../controllers/setting.controller.js");
 
 router.get('/browser_source_url', async (req, res) => {
-	settings = await settingsCtrl.getSettings(req.session.user.id);
+  const user_id = req.query.room
+	settings = await settingsCtrl.getSettings(user_id);
   res.render('views/browser_source_url', {settings: settings});
 });
 
 router.get('/browser_source', async (req, res) => {
-	settings = await settingsCtrl.getSettings(req.session.user.id);
+  const user_id = req.query.room
+	settings = await settingsCtrl.getSettings(user_id);
   res.render('views/browser_source', {settings: settings});
 });
 
