f8f608063c82cdb8b442049cc1a1cc4bbb297cbf
Max St-Onge <maxstonge@gmail.com>
2020-10-31 13:27:50 -0400

    Ensure user has tweet access

--- app/routes/tweets.js
+++ app/routes/tweets.js
@@ -5,16 +5,16 @@ const auth = require("../controllers/auth.controller.js");
 const tweetsCtrl = require("../controllers/tweets.controller.js");
 const settingsCtrl = require("../controllers/setting.controller.js");
 
-router.get('/tweets/add', (req, res) => {
+router.get('/tweets/add', auth.hasTweetAccess, (req, res) => {
   res.render('views/add_tweet');
 });
 
-router.post('/tweets/add', async (req, res) => {
+router.post('/tweets/add', auth.hasTweetAccess, async (req, res) => {
   await tweetsCtrl.create(req, res);
   res.redirect('/tweets')
 });
 
-router.post('/tweets_custom/add', async (req, res) => {
+router.post('/tweets_custom/add', auth.hasTweetAccess, async (req, res) => {
   await tweetsCtrl.create(req, res);
   res.redirect('/tweets_custom')
 });
@@ -25,7 +25,7 @@ router.get('/tweets_custom', auth.hasTweetAccess, async (req, res) => {
   res.render('views/tweets_custom', {tweets: tweets, settings: settings, libs: ['tweets_custom']});
 });
 
-router.post('/tweets_custom/delete', (req, res) => {
+router.post('/tweets_custom/delete', auth.hasTweetAccess, (req, res) => {
   const tweets = req.body.tweets;
   var tweet_ids = []
   tweets.forEach(async function(tweet) {
@@ -36,7 +36,7 @@ router.post('/tweets_custom/delete', (req, res) => {
   res.sendStatus(200)
 });
 
-router.post('/tweets/delete', (req, res) => {
+router.post('/tweets/delete', auth.hasTweetAccess, (req, res) => {
   const tweets = req.body.tweets.map(BigInt);
   tweetsCtrl.destroy(tweets, req.session.user.id);
 
